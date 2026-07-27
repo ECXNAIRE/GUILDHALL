ac7daed51005300a6cba8584e3ec2cf06e91668e
suryansh <suryanshsinghrajput007@gmail.com>
2026-07-27 21:43:18 +0530

    implemented the quest content display

--- static/js/questDisplay.js
+++ static/js/questDisplay.js
@@ -2,10 +2,27 @@ import { state } from "./state.js"
 
 const displayPopup = document.getElementById("questDisplayPopup")
 const overlay = document.getElementById("overlay")
-
+const titleDisplay = document.getElementById("titleDisplay")
+const descriptionDisplay = document.getElementById("descriptionDisplay")
+const difficultyDisplayid = document.getElementById("difficultyDisplayid")
+const tagBoxDisplay = document.getElementById("tagBoxDisplay")
 
 export function displayQuest(quest) {
     if (state.selectedQuest) {
+        titleDisplay.textContent = state.selectedQuest.title
+        descriptionDisplay.textContent = state.selectedQuest.description
+        difficultyDisplayid.textContent = state.selectedQuest.difficulty
+        tagBoxDisplay.innerHTML = "";
+        state.selectedQuest.tags.forEach(tag => {
+            const tagDiv = document.createElement("div");
+            tagDiv.classList.add("tagDisplay");
+            tagDiv.textContent = tag;
+
+            tagBoxDisplay.appendChild(tagDiv);
+
+        });
+
+
         displayPopup.style.display = "block"
         overlay.style.display = "block"
     }
