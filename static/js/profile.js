065b59dc5ff1f5c05385e427444f7bf195e73e0e
suryansh <suryanshsinghrajput007@gmail.com>
2026-07-28 10:22:42 +0530

    added skill selection

---
+++ static/js/profile.js
@@ -0,0 +1,72 @@
+import { state } from "./state.js";
+
+
+const skillsSearch = document.getElementById("skillsSearch")
+const suggestionBox = document.getElementById("skills-suggestions")
+const selectedBox = document.getElementById("selectedskills")
+
+
+const allSkills = [
+    "Python",
+    "JavaScript",
+    "Frontend",
+    "Backend",
+    "React",
+    "Flask",
+    "Django",
+    "HTML",
+    "CSS",
+    "AI",
+    "Machine Learning",
+    "Database",
+    "DevOps",
+    "Mobile Dev"
+]
+
+
+let selectedSkills = []
+function renderSuggestions() {
+    suggestionBox.innerHTML = ""
+
+    const search = skillsSearch.value.toLocaleLowerCase()
+
+
+    allSkills.filter(skill => skill.toLocaleLowerCase().includes(search) && !selectedSkills.includes(skill)).forEach(skill => {
+        const div = document.createElement("div")
+
+        div.className = "suggestions"
+        div.textContent = skill
+
+        div.onclick = () => {
+            selectedSkills.push(skill)
+            renderSuggestions()
+            renderSelectedSkills()
+        }
+
+        suggestionBox.appendChild(div)
+    })
+
+}
+
+renderSuggestions()
+
+
+function renderSelectedSkills() {
+    selectedBox.innerHTML = ""
+
+    selectedSkills.forEach(skill => {
+        const chip = document.createElement("div")
+        chip.className = "skill"
+        chip.textContent = skill
+        chip.onclick = () => {
+            selectedSkills = selectedSkills.filter(t => t !== skill)
+
+            renderSelectedSkills()
+            renderSuggestions()
+        }
+
+        selectedBox.appendChild(chip)
+    })
+}
+
+skillsSearch.addEventListener("input", renderSuggestions)
